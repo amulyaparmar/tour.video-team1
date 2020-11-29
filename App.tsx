fe4255fa0893b0110929a26dae37a3b9d219cde7
jenniferl00 <73903034+jenniferl00@users.noreply.github.com>
2020-11-28 18:15:38 -0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,126 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useRef } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, TextInput } from 'react-native';
+import { Platform, Picker, StyleSheet, Text, View, Button, ScrollView, TextInput, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Modal } from 'react-native';
 import { v4 as uuidv4 } from 'uuid'
+//import Modal from 'react-native-modal'
+//import {Picker} from '@react-native-picker/picker';
 
 import { Video } from 'expo-av'
 import VideoPlayer from 'expo-video-player'
 export default function App() {
 
-  const [video, videoList] = useState([])
-  const newName = useRef()
-  const newID = useRef()
-  const newURL = useRef()
-  const newCategory = useRef()
+  interface videoEntity {
+    "name" : string,
+    "id" : string,
+    "url" : string,
+    "category" : string,
+  }
 
-  function addVideo(e) {
+  /*
+
+  Welcome 
+  video.filter((vid)=> vid.category === "Welcome").map((vid, idx )  ...
+
+  Miscellaneous 
+  video.filter((vid)=> vid.category === "Miscellaneous").map((vid, idx )  ...
+
+  */
+
+  const [video, setVideoList] = useState<any[]>([])
+  const newCategory = useRef()
+  const [newObject, setNewObject] = useState({
+    name: '',
+    id: '',
+    url: '',
+    category: ''
+  })
+
+  const categories = ['Welcome', 'Floor Plan', 'Amenities', 'Thank You', 'Miscellaneous']
+
+  // We built
+  /*
+  1. This view for video list  & categories
+  2. Styling fo rthe project, that you want to carry through to the rest of the app
+  3. You want to make when you clikc an video /// video editing screen
+  4. Additionally, you want a nice navigation for new video 
+  */
+
+  const renderNewInput=()=> {
+    return(
     <>
     <Text>Video name: </Text>
     <TextInput
-      onChangeText={text => setText(text)}
-      value={text}
+      onChangeText={text => setNewObject({...newObject, 'name': text})}
+      value={newObject['name']}
+      style={styles.inputBox}
+    />
+    <Text>Id: </Text>
+    <TextInput
+      style={styles.inputBox}
+      value={newObject['id']}
     />
     <Text>Url: </Text>
     <TextInput
-      onChangeText={text => setText(text)}
-      value={text}
+      onChangeText={text => setNewObject({...newObject, 'url': text})}
+      value={newObject['url']}
+      style={styles.inputBox}
     />
-    const nameInput = newName.current.value
-    const urlInput = newURL.current.value
 
-    if (nameInput === '') return
-    videoList(preVideo => {
-      return [...preVideo, {name: nameInput, id: uuidv4(), url: urlInput}]
-    })
+    <Picker onValueChange={(itemValue, itemIndex) => setNewObject({...newObject, 'category': itemValue})}>
+      {categories.map((category, index)=>{
+        return(
+          <Picker.Item label={category} value={category}>
+          </Picker.Item>
+        )
+      })}
+    </Picker>
+
+    <Button onPress={addVideo}
+      title='Finish'
+    />
     </>
+    )
   }
 
-  // video categories
+   
+  function addVideo() {
+    const nameInput = newObject['name']
+    //const IDInput = uuidv4()
+    const urlInput = newObject['url']
+
+    if (nameInput === '') return;
+    setVideoList([...video, {"name": nameInput, "id": uuidv4(), "url": "https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f716fa43c67c778b286e5e3_Sample%20sizze%20reel%20(1)-transcode.mp4", "category": "Welcome"}])
+
+    setNewObject({name: '',
+    id: '',
+    url: '',
+    category: ''})
+  }
+
+
+  {/* FOR MODAL POP-UP WINDOW
+  function WrapperComponent() {
+    return(
+      <View>
+        <Modal isVisible={true}>
+          <View
+            style={{ flex: 1 }}>
+            <Text>I am the modal content!</Text>
+          </View>
+        </Modal>
+      </View>
+    )
+  }
+
+  function ModalTester() {
+    const [isModalVisible, setModalVisible] = useState(false);
+    
+    const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+    }
+  }
+
+*/}
   const [introduction, setVideos1] = React.useState(
     [
       {
@@ -104,100 +189,132 @@ export default function App() {
 
   return (
     <ScrollView style={styles.WebViewContainer}>
-    <View style={styles.redView}>
-      <Text style={styles.welcome}>LeaseMagnets Welcome</Text>
-    </View>
+      <View style={styles.redView}>
+        <Text style={styles.welcome}>LeaseMagnets Welcome</Text>
+      </View>
       
-    <Text style={styles.section}>{"\n"}WELCOME{"\n"}</Text>
-    {introduction.map(((video, idx) => {
-      return (
-        <>
-        <View style={styles.container}>
-          <View style={styles.column}>
-            <View style={styles.buttonTitle}>
-              <Button title={video.name}/>
-            </View>
-            <Text>{video.name}</Text>
-          </View>
+      {/* SEARCH BOX */}
+      <View style={styles.searchBox}>
+        <View style={styles.searchText}>
+          <TextInput
+            placeholder='  Search'>
+          </TextInput>
         </View>
-        </>
-      )
-    }))}
-    
+      </View>
+
+      {/* WELCOME SECTION */}
+      <Text style={styles.section}>{"\n"}WELCOME{"\n"}</Text>
+      {video.filter((vid)=> vid.category === "Welcome").map((vid, idx )  => {
+            return (
+              <>
+              <View key={idx} style={styles.container}>
+                <View style={styles.column}>
+                  <View style={styles.buttonTitle}>
+                    <Button title={vid.name}/>
+                  </View>
+                <Text>{vid.name}</Text>
+                </View>
+              </View>
+              </>
+            );
+
+
+      })}
+     {/* {introduction.map(((video, idx) => { 
+      //   return (
+      //     <>
+      //     <View key={idx} style={styles.container}>
+      //       <View style={styles.column}>
+      //         <View style={styles.buttonTitle}>
+      //           <Button title={video.name}/>
+      //         </View>
+      //       <Text>{video.name}</Text>
+      //       </View>
+      //     </View>
+      //     </>
+      //   )
+      // }))} 
+      */}
+      
+
+      {/* FLOOR PLAN SECTION */}
       <Text style={styles.horizontal_lines}>______________________________________________</Text>
       <Text style={styles.section}>FLOOR PLAN{"\n"}</Text>
       <View style={styles.container}>
         {floor_plan.map(((video, idx) => {
           return (
             <>
-            <View style={styles.container}>
+            <View key={idx}  style={styles.container}>
               <View style={styles.column}>
                 <View style={styles.videoView}>
-                <VideoPlayer
-                  width= {100}
-                  height= {125}
-                  videoProps={{
-                    shouldPlay: true,
-                    resizeMode: Video.RESIZE_MODE_CONTAIN,
-                    source: {
-                      uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',
-                    },
-                  }}
-                  inFullscreen={false}
-                />
-                  <Text style={styles.names}>{video.name}</Text>
+                  <VideoPlayer
+                    width= {100}
+                    height= {125}
+                    videoProps={{
+                      shouldPlay: true,
+                      resizeMode: Video.RESIZE_MODE_CONTAIN,
+                      source: {
+                        uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',
+                        },
+                      }}
+                    inFullscreen={false}
+                  />
                 </View>
+                <Text>{video.name}</Text>
               </View>
             </View>
             </>
-          )
-        }))}
+            )
+          }))}
       </View>
 
+
+      {/* AMENITIES SECTION */}
       <Text style={styles.horizontal_lines}>______________________________________________</Text>
       <Text style={styles.section}>AMENITIES{"\n"}</Text>
       <View style ={styles.container}>
-      {amenities.map(((video, idx) => {
+        {amenities.map(((video, idx) => {
         return (
-          <>
-          <View style ={styles.container}>
+          
+          <View key={idx}  style ={styles.container}>
             <View style={styles.column}>
               <View style={styles.videoView}>
-              <VideoPlayer
-                width={100}
-                height={125}
-                videoProps={{
-                  shouldplay: true,
-                  resizeMode: video.RESIZE_MODE_CONTAIN,
-                  source:{
-                    uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',   
-                  },
-                }}
+                <VideoPlayer
+                  width={100}
+                  height={125}
+                  videoProps={{
+                    shouldplay: true,
+                    resizeMode: video.RESIZE_MODE_CONTAIN,
+                    source:{
+                      uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',   
+                    },
+                  }}
                 inFullscreen={false}
-              />
+                />
                 <Text style={styles.names}>{video.name}</Text>
               </View>
             </View>
           </View>
-          </>
+
         );
-      }))}
+        }))}
       </View>
       
+
+      {/* THANK YOU SECTION */}
       <Text style={styles.horizontal_lines}>______________________________________________</Text>
       <Text style={styles.section}>THANK YOU{"\n"}</Text>
       <View style={styles.container}>
       {thank_you.map(((video, idx) => {
         return (
-          <>
-          <View style ={styles.container}>
+          <View key={idx} style ={styles.container}>
             <View style={styles.column}>
-               <View style={styles.videoView}>
+              <View style={styles.videoView}>
                 <VideoPlayer  
                   width= {100}
                   height={125}
                   videoProps={{
-                    shouldPlay: true,
+                    shouldPlay: false,
                     resizeMode: Video.RESIZE_MODE_CONTAIN,
                     source: {
                       uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4', 
@@ -209,55 +326,103 @@ export default function App() {
               </View>
             </View>
           </View>
-          </>
         )
       }))}
       </View>
 
+
+      {/* MISCELLANEOUS SECTION */}
       <Text style={styles.horizontal_lines}>______________________________________________</Text>
       <Text style={styles.section}>MISCELLANEOUS{"\n"}</Text>
       <View style={styles.container}>
       {miscellaneous.map(((video, idx) => {
         return (
-          <>
           <View style ={styles.container}>
             <View style={styles.column}>
               <View style={styles.videoView}>
-              <VideoPlayer 
-                width= {100}
-                height= {125}
-                videoProps={{
-                  shouldPlay: true,
-                  resizeMode: Video.RESIZE_MODE_CONTAIN,
-                  source: {
-                    uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',
-                  },
-                }} 
+                <VideoPlayer 
+                  width= {100}
+                  height= {125}
+                  videoProps={{
+                    shouldPlay: true,
+                    resizeMode: Video.RESIZE_MODE_CONTAIN,
+                    source: {
+                      uri: 'https://uploads-ssl.webflow.com/5e45b17b74507185e599a039/5f95fe348b955b74d1fb8704_commons-intro-transcode.mp4',
+                    },
+                  }} 
                 inFullscreen={false}
-              />
+                />
                 <Text style={styles.names}>{video.name}</Text>
               </View>
             </View>
           </View>
-          </>
         )
       }))}
       </View>
-
-      <StatusBar style="auto" />
-      <Text style={styles.horizontal_lines}>______________________________________________</Text>
-      <View style = {styles.container}>
-        <View style={styles.addButton}>
-          <Button 
-            style = {styles.plus} 
-            title="+"></Button>
-        
-        </View>
+      {/* <NewInput /> */}
+      {renderNewInput()}
+    <StatusBar style="auto" />
+    <Text style={styles.horizontal_lines}>______________________________________________</Text>
+    <View style = {styles.container}>
+      
+      <View style={styles.addButton}>
+        <Button 
+          style = {styles.plus} 
+          title="+"></Button>
       </View>
-        <Text>Add New Floor Plan</Text>
-  </ScrollView>
-  );
-}
+    </View>
+    <Button 
+      onPress={addVideo}
+      title="Add a video">    
+    </Button>
+    <Text>Add New Floor Plan</Text>
+    {/*
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <>
+        <Text>Video name: </Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder= 'e.g. Main Bedroom'
+        />
+        </>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+    */}
+      {/*
+    onChangeText={text => setText(text)}
+    value={text}
+
+    <Text>Url: </Text>
+    <TextInput
+      onChangeText={text => setText(text)}
+      value={text}
+    />
+    <Button onClick={addVideo}>Finish</Button>
+    */}
+    
+  
+    {/* ERR0RS WITH TOGGLE MODAL
+      <View style={{flex: 1}}>
+        <Button 
+          title = "Show modal"
+          onPress={toggleModal}
+        />
+        <Modal isVisible={isModalVisible}>
+          <View style={{flex: 1}}>
+            <Text>Hello!</Text>
+            <Button 
+              title="Hide modal"
+              onPress={toggleModal}
+            />
+          </View>
+        </Modal>
+      </View>
+    */}
+    </ScrollView> 
+  )
+  };
 
 const styles = StyleSheet.create({
   container: {
@@ -305,24 +470,16 @@ const styles = StyleSheet.create({
   section: {
     justifyContent: 'flex-start',
     alignItems: 'center',
-    //transform: [{rotate: '270deg'}],
     flex: 1,
     flexDirection: 'row',
-    fontSize: 15,
+    fontSize: 16,
     paddingLeft: 10,
     marginLeft: 10,
     marginTop: 1,
     fontWeight: 'bold',
   },
-  vertical_lines: {
-    //borderWidth: 0.5,
-    //borderColor:'black',
-    borderLeftWidth: 1,
-    borderLeftColor: 'black',
-    margin: 20
-  },
   horizontal_lines: {
-    margin: 10,
+    margin: 7,
   }, 
   welcome: {
     fontWeight:"bold",
@@ -334,7 +491,6 @@ const styles = StyleSheet.create({
   names: {
     justifyContent: 'center',
     alignItems: 'center',
-    //transform: [{rotate: '270deg'}],
     flex: 1,
     flexDirection: 'row',
     fontSize: 15,
@@ -352,5 +508,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 10,
     fontSize: 10,
+  },
+  inputBox: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+  searchBox: {
+    borderWidth: 1,
+    backgroundColor: 'rgb(234, 236, 238)',
+    borderColor: 'rgb(234, 236, 238)',
+    width: 345,
+    margin: 15,
+    borderRadius: 7,
+    height: 35,
+  },
+  searchText: {
+    fontSize: 35,
+    color: 'rgb(128, 139, 150)',
+    margin: 8,
   },
 });
